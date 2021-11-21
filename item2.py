5e1a0e57c9fe39e536525fed5fe26b38f4231326
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2021-11-20 23:40:30 -0300

    add item 5

---
+++ item2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Faça uma função que informe a quantidade de dígitos de um determinado número inteiro informado.
+
+
+def contarDigitos(numero):
+    return len(str(numero))
+
+
+print(contarDigitos(266))
